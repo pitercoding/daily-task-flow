0ea3c0b46d3104eb040669827c251b8be7015132
pitercoding <piterg.bio@gmail.com>
2026-04-14 09:07:15 +0200

    update favicon and page title

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class App {}
